0229c851443540aad43f6cd70474209a4c8e5a19
Vrancean <71001705+Vrancean@users.noreply.github.com>
2021-12-08 20:16:09 +0200

    Add files via upload

---
+++ aplicatie_practica.py
@@ -0,0 +1,27 @@
+with open('D:abc.txt', 'r') as f:
+    list1=f.readlines()
+m=0
+y=0
+u=0
+me1=0
+me2=0
+for i in list1:
+    lista2=i.split()
+    m+=int(lista2[2])
+    if lista2[3]=='Eng1':
+        me1+=int(lista2[2])
+        y+=1
+        with open('D:ghjkhg.txt','a') as f:
+            x=lista2[0]+' '+lista2[1]+'\n'
+            f.write(x)
+    else:
+        me2+=int(lista2[2])
+        u+=1
+        with open('D:ghjhgfd.txt','a') as f:
+            x=lista2[0]+' '+lista2[1]+'\n'
+            f.write(x)
+with open('D:ghjkhg.txt','a') as f:
+    f.write('Media grupului 1: '+str(me1/y))
+with open('D:ghjhgfd.txt','a') as f:
+    f.write('Media grupului 2: '+str(me2/u))
+print('Media clasei : ',m/len(list1))
